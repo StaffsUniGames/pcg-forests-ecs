f873c43bee55d16b2280021a32bda85b2733d11b
Davin Ward <mrdaveoso@hotmail.co.uk>
2024-06-14 15:59:10 +0100

    Scene update

--- Assets/ecs/systems/DataCollectionSystem.cs
+++ Assets/ecs/systems/DataCollectionSystem.cs
@@ -1,94 +1,94 @@
-using UnityEngine;
-using Unity.Burst;
-using Unity.Jobs;
-using Unity.Entities;
-using UnityEditor;
-using System.IO;
+//using UnityEngine;
+//using Unity.Burst;
+//using Unity.Jobs;
+//using Unity.Entities;
+//using UnityEditor;
+//using System.IO;
 
-[UpdateBefore(typeof(ForestUpdateSystem))]
-[BurstCompile]
-public partial struct DataCollectionSystem : ISystem
-{
-	[BurstCompile]
-	public void OnCreate(ref SystemState state)
-	{
-		state.RequireForUpdate<DataCollectionBuffer>();
-		state.RequireForUpdate<ForestComponent>();
+//[UpdateBefore(typeof(ForestUpdateSystem))]
+//[BurstCompile]
+//public partial struct DataCollectionSystem : ISystem
+//{
+//	[BurstCompile]
+//	public void OnCreate(ref SystemState state)
+//	{
+//		state.RequireForUpdate<DataCollectionBuffer>();
+//		state.RequireForUpdate<ForestComponent>();
 
-#if UNITY_EDITOR
-		if (!EditorApplication.isPlaying) { return; }
-#endif
-		foreach (RefRO<ForestComponent> forest in SystemAPI.Query<RefRO<ForestComponent>>())
-		{
-			StreamWriter writer = File.CreateText(forest.ValueRO.m_filePath.Value.path.ToString());
-			writer.WriteLine("GridSubdivisions,EntityCount,deltaTime");
-			writer.Flush();
-			writer.Close();
-		}
-	}
+//#if UNITY_EDITOR
+//		if (!EditorApplication.isPlaying) { return; }
+//#endif
+//		foreach (RefRO<ForestComponent> forest in SystemAPI.Query<RefRO<ForestComponent>>())
+//		{
+//			StreamWriter writer = File.CreateText(forest.ValueRO.m_filePath.Value.path.ToString());
+//			writer.WriteLine("GridSubdivisions,EntityCount,deltaTime");
+//			writer.Flush();
+//			writer.Close();
+//		}
+//	}
 
-	private void SaveOutData(ForestComponent forest, ref DynamicBuffer<DataCollectionBuffer> buff)
-	{
-		//save out the date to csv
-		StreamWriter writer = new StreamWriter(forest.m_filePath.Value.path.ToString(), append: true);
-		for (int i = 0; i < buff.Length; i++)
-		{
-			writer.WriteLine($"{buff[i].gridSubdivisions},{buff[i].treeCount},{buff[i].deltaTime}");
-		}
-		writer.Flush();
-		writer.Close();
-	}
+//	private void SaveOutData(ForestComponent forest, ref DynamicBuffer<DataCollectionBuffer> buff)
+//	{
+//		//save out the date to csv
+//		StreamWriter writer = new StreamWriter(forest.m_filePath.Value.path.ToString(), append: true);
+//		for (int i = 0; i < buff.Length; i++)
+//		{
+//			writer.WriteLine($"{buff[i].gridSubdivisions},{buff[i].treeCount},{buff[i].deltaTime}");
+//		}
+//		writer.Flush();
+//		writer.Close();
+//	}
 
-	[BurstCompile]
-	public void OnUpdate(ref SystemState state)
-	{
-		DynamicBuffer<DataCollectionBuffer> buff = SystemAPI.GetSingletonBuffer<DataCollectionBuffer>();
+//	[BurstCompile]
+//	public void OnUpdate(ref SystemState state)
+//	{
+//		DynamicBuffer<DataCollectionBuffer> buff = SystemAPI.GetSingletonBuffer<DataCollectionBuffer>();
 
-		EntityQuery treeQuery = SystemAPI.QueryBuilder().WithAll<TreeComponent>().Build();
+//		EntityQuery treeQuery = SystemAPI.QueryBuilder().WithAll<TreeComponent>().Build();
 
-		bool finished = false;
+//		bool finished = false;
 
-		foreach (RefRW<ForestComponent> forest in SystemAPI.Query<RefRW<ForestComponent>>())
-		{
-			buff.Add(new DataCollectionBuffer
-			{
-				gridSubdivisions = forest.ValueRO.m_spatialHasher.m_gridSize,
-				deltaTime = SystemAPI.Time.DeltaTime,
-				treeCount = treeQuery.CalculateEntityCount()
-			});
+//		foreach (RefRW<ForestComponent> forest in SystemAPI.Query<RefRW<ForestComponent>>())
+//		{
+//			buff.Add(new DataCollectionBuffer
+//			{
+//				gridSubdivisions = forest.ValueRO.m_spatialHasher.m_gridSize,
+//				deltaTime = SystemAPI.Time.DeltaTime,
+//				treeCount = treeQuery.CalculateEntityCount()
+//			});
 
-			if(buff.Length >= 1000)
-			{
-				SaveOutData(forest.ValueRO, ref buff);
+//			if(buff.Length >= 1000)
+//			{
+//				SaveOutData(forest.ValueRO, ref buff);
 
-				if (forest.ValueRO.m_spatialHasher.m_gridSize >= 256)
-				{
-					finished = true;
-					break;
-				}
+//				if (forest.ValueRO.m_spatialHasher.m_gridSize >= 256)
+//				{
+//					finished = true;
+//					break;
+//				}
 
-				buff.Clear();
+//				buff.Clear();
 
-				forest.ValueRW.m_spatialHasher.m_gridSize++;
-				forest.ValueRW.m_InitialSeed = true;
+//				forest.ValueRW.m_spatialHasher.m_gridSize++;
+//				forest.ValueRW.m_InitialSeed = true;
 
-				buff.Add(new DataCollectionBuffer
-				{
-					gridSubdivisions = forest.ValueRO.m_spatialHasher.m_gridSize,
-					deltaTime = 0f,
-					treeCount = 0
-				});
+//				buff.Add(new DataCollectionBuffer
+//				{
+//					gridSubdivisions = forest.ValueRO.m_spatialHasher.m_gridSize,
+//					deltaTime = 0f,
+//					treeCount = 0
+//				});
 
-				new ResetForestJob
-				{
-					ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter()
-				}.ScheduleParallel();
-			}
-		}
+//				new ResetForestJob
+//				{
+//					ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter()
+//				}.ScheduleParallel();
+//			}
+//		}
 
-		if(finished)
-		{
-			EditorApplication.ExitPlaymode();
-		}
-	}
-}
+//		if(finished)
+//		{
+//			EditorApplication.ExitPlaymode();
+//		}
+//	}
+//}
